31dac9a2cb009415a29657e1f46bf47cc0e4d4c0
Raul Reis <raulgomesreis@gmail.com>
2024-05-28 22:44:42 -0300

    Criação de rota/model/controller para quiz

---
+++ src/controllers/quizController.js
@@ -0,0 +1,74 @@
+var quizModel = require("../models/quizModel");
+// var aquarioModel = require("../models/aquarioModel");
+
+function autenticar(req, res) {
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else {
+
+        quizModel.autenticar(email, senha)
+            .then(
+                function (resultadoAutenticar) {
+                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+                    } else if (resultadoAutenticar.length == 0) {
+                        res.status(403).send("Email e/ou senha inválido(s)");
+                    } else {
+                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    } res.status(200).json({ok: true});
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var idUsuario = req.body.idUserServer;
+    var pontuacao = req.body.pontuacaoServer;
+    // var senha = req.body.senhaServer;
+
+    // Faça as validações dos valores
+    if (idUsuario == undefined) {
+        res.status(400).send("Seu usuário está undefined!");
+    } else if (pontuacao == undefined) {
+        res.status(400).send("Sua pontuação está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo quizModel.js
+        quizModel.cadastrar(idUsuario, pontuacao)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+}
---
+++ src/routes/quiz.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+var quizController = require("../controllers/quizController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de quizController.js
+router.post("/cadastrarQuiz", function (req, res) {
+    quizController.cadastrar(req, res);
+})
+
+router.post("/autenticarQuiz", function (req, res) {
+    quizController.autenticar(req, res);
+});
+
+module.exports = router;
