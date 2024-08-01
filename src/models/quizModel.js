366ac3c42506d139174d8d6ce80045867832b037
Raul Reis <raulgomesreis@gmail.com>
2024-08-01 14:49:19 -0300

    Query reformulada para nova consulta utilizando MIN()

--- src/models/quizModel.js
+++ src/models/quizModel.js
@@ -12,7 +12,7 @@ function obterDados(idUSer) {
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(idUsuario, pontuacao) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarQuiz():", idUsuario, pontuacao);
-    
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
@@ -27,10 +27,31 @@ function ranking() {
     console.log('Model quiz acessada, Função ranking executada');
 
     // Comando ORDER BY adicionado para exibir ranking do maior para o menor 12/07/2024
+    // Reformulando query para consulta de novos dados 01/08/2024
     var instrucaoSql = `
-    SELECT usuario.nome, MAX(quiz.pontuation) AS pontuation FROM usuario
-        JOIN quiz ON id = fkUser GROUP BY nome
-            ORDER BY pontuation DESC;
+
+    -- Subconsulta para as 10 maiores pontuações
+    (SELECT usuario.nome, MAX(quiz.pontuation) AS pontuation, 'maior' AS tipo
+     FROM usuario
+     JOIN quiz ON usuario.id = quiz.fkUser 
+     GROUP BY usuario.nome
+     ORDER BY pontuation DESC
+     LIMIT 10)
+    
+    UNION ALL
+
+    -- Subconsulta para as 10 menores pontuações
+    (SELECT usuario.nome, MAX(quiz.pontuation) AS pontuation, 'menor' AS tipo
+     FROM usuario
+     JOIN quiz ON usuario.id = quiz.fkUser 
+     GROUP BY usuario.nome
+     ORDER BY pontuation ASC
+     LIMIT 10);
+
+
+    -- SELECT usuario.nome, MAX(quiz.pontuation) AS pontuation FROM usuario
+    --     JOIN quiz ON id = fkUser GROUP BY nome
+    --   ORDER BY pontuation DESC;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
